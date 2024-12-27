cc543b124ba8a5addc4c27feb021212e7e5b6bac
Matt McGovern <mattmcgovern@gmail.com>
2024-12-27 15:44:14 +0000

    Adding in additional datasets

--- data_review_app.R
+++ data_review_app.R
@@ -1,10 +1,15 @@
 library(teal.modules.general)
 
-ADSL <- pharmaverseadam::adsl
+#ADSL <- pharmaverseadam::adsl
+
+ADTTE <- pharmaverseadam::adtte_onco
 
 app <- teal::init(
   data = cdisc_data(
-    ADSL = example_cdisc_data("ADSL")
+    ADSL = example_cdisc_data("ADSL"),
+    ADAE = example_cdisc_data("ADAE"),
+    ADTTE = example_cdisc_data("ADTTE"),
+    verify = TRUE
   ),
   modules = teal::modules(
     tm_data_table()
